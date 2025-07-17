47de8324646cfae2523f02b312176c8e8442da78
alefaso-lucky <ale.fasolino02@gmail.com>
2025-07-17 18:15:56 +0200

    aggiunta stampe

--- src/main.py
+++ src/main.py
@@ -30,6 +30,7 @@ certificate_authority = CertificateAuthority("ca_01")
 
 
 # --- FASE A: INIZIO COMUNICAZIONE STUDENTE - UNIVERSITA' ---
+print("== FASE A == INIZIO COMUNICAZIONE STUDENTE - UNIVERSITA' ==\n")
 
 # set-up informazioni per la comunicazione asimmetrica
 student.set_up_asymmetric_communication_keys()
@@ -39,19 +40,51 @@ university_of_origin.set_up_asymmetric_communication_keys()
 university_of_origin.ask_for_certificate_of_identity(certificate_authority)
 
 # scambio certificati di identità
+print("=== MESSAGGIO 1 ===")
+print("Mittente     :   Studente")
+print("Destinatario :   Università")
+print("Descrizione  :   Vertificato firmato dalla CA")
+print("Contenuto    :   C_Stu = E(PR_{auth}, [T_1 || ID_Stu || PU_Stu])\n")
 student_certificate = student.send_certificate_of_identity()
 university_of_origin.receive_certificate_of_identity(student_certificate)
 
+print("=== MESSAGGIO 2 ===")
+print("Mittente     :   Università")
+print("Destinatario :   Studente")
+print("Descrizione  :   Certificato firmato dalla CA")
+print("Contenuto    :   C_U = E(PR_{auth}, [T_2 || ID_U || PU_U])\n")
 university_certificate = university_of_origin.send_certificate_of_identity()
 student.receive_certificate_of_identity(university_certificate)
 
 # protocollo di distribuzione sicura della chiave
+print("=== MESSAGGIO 3 ===")
+print("Mittente     :   Università")
+print("Destinatario :   Studente")
+print("Descrizione  :   Inizio della challenge - mutual authentication protocol")
+print("Contenuto    :   E(PU_Stu, [ID_U || Nonce_1])\n")
 first_message = university_of_origin.secure_key_distribution_protocol_send_first_message()
+
+print("=== MESSAGGIO 4 ===")
+print("Mittente     :   Studente")
+print("Destinatario :   Università")
+print("Descrizione  :   Risposta alla challenge e invio sfida di autenticazione all'università")
+print("Contenuto    :   E(PU_U, [Nonce_1 || Nonce_2])\n")
 second_message = student.secure_key_distribution_protocol_receive_first_message_and_send_second_message(first_message)
+
+print("=== MESSAGGIO 5 ===")
+print("Mittente     :   Università")
+print("Destinatario :   Studente")
+print("Descrizione  :   Conclusione autenticazione reciproca")
+print("Contenuto    :   E(PU_Stu, Nonce_2)\n")
 third_message = university_of_origin.secure_key_distribution_protocol_receive_second_message_and_send_third_message(second_message)
 student.secure_key_distribution_protocol_receive_third_message(third_message)
 
 # scambio della chiave di sessione
+print("=== MESSAGGIO 6 ===")
+print("Mittente     :   Università")
+print("Destinatario :   Studente")
+print("Descrizione  :   distribuzione chiave simmetrica")
+print("Contenuto    :   E(PU_Stu, E(PR_U, K_S))\n\n")
 university_of_origin.set_up_symmetric_communication()
 session_info_encrypted = university_of_origin.send_information_symmetric_communication()
 
@@ -59,8 +92,21 @@ session_info_decrypted = student.decrypt_and_verify_message_asymmetric_encryptio
 student.set_up_symmetric_communication_from_info_received(session_info_decrypted)
 
 # --- FASE B: RICHIESTA CERTIFICATO ALL'UNIVERSITA' ---
+print("== FASE B == RICHIESTA CERTIFICATO ALL'UNIVERSITA' ==\n")
+
+print("=== MESSAGGIO 1 ===")
+print("Mittente     :   Studente")
+print("Destinatario :   Università")
+print("Descrizione  :   Richiesta credenziali\n")
 student_certificate_request = student.ask_for_student_info_certificate()
+
 signature_of_tree_root, tree = university_of_origin.receive_student_info_certificate_request(student_certificate_request)
+
+print("=== MESSAGGIO 2 ===")
+print("Mittente     :   Università")
+print("Destinatario :   Studente")
+print("Descrizione  :   Invio credenziali con Merkle Tree per verificarne l'autenticità")
+print("Contenuto    :   E(K_S, MerkleTree||E(K_U, RadiceMerkleTree))\n")
 student_certificate = [signature_of_tree_root, tree]
 
 # --- FASE C: INVIO CERTIFICATO ALL'UNIVERSITA' ---
